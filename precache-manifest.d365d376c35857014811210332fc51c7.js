67da0d20d4522d292f5b89329e0450c62229e6dd
VeronikaMishkovec <veronikakireenko@gmail.com>
2020-06-11 10:54:30 +0300

    Updates

---
+++ precache-manifest.d365d376c35857014811210332fc51c7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "810fe6e1284d0a37e129ed91ebb8dd65",
+    "url": "/Demo-qr-scan/index.html"
+  },
+  {
+    "revision": "2159555dc0cb1b984c79",
+    "url": "/Demo-qr-scan/static/css/main.150bb46f.chunk.css"
+  },
+  {
+    "revision": "aa9a011ec15cc0bedf70",
+    "url": "/Demo-qr-scan/static/js/2.8c7a2a2d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Demo-qr-scan/static/js/2.8c7a2a2d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2159555dc0cb1b984c79",
+    "url": "/Demo-qr-scan/static/js/main.1649239f.chunk.js"
+  },
+  {
+    "revision": "e80c96b5e11bcfb8c6c3",
+    "url": "/Demo-qr-scan/static/js/runtime-main.f548ac27.js"
+  }
+]);
